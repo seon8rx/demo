2eaea42c51f8db11d73f3ef76fa7cd9144dc30af
SUNGKYU KIM <seooonggyu@gmail.com>
2024-09-10 19:48:44 +0900

    24-02 spring session (09/09-09/10)

--- src/main/java/com/example/demo/controller/BoardRestController.java
+++ src/main/java/com/example/demo/controller/BoardRestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Board;
+import com.example.demo.service.BoardService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +16,26 @@ import java.util.Map;
 @RestController
 public class BoardRestController {
 
+
+    private final BoardService boardService;;
+    public BoardRestController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+
+
     List<Map<String, Object>> boardList = new ArrayList<>();
 
     @GetMapping("/create") // 이 안에 있는 주소값은 꼭 유니크해야함!! (클래스 안에서만)
     public Map<String, Object> create(@RequestParam Map<String, Object> params) {
 
+        return boardService.createBoard(params);
+
+//        Map<String, Object> resultMap = new HashMap<>();
+//        resultMap.put("resultCode", 200);
+
+        /*
+        이전에 컨트롤러 필드에 저장해보는 코드
         String title = (String) params.get("title");
         String content = params.get("content") + "";
         String author = params.get("author") + "";
@@ -32,16 +49,13 @@ public class BoardRestController {
         boardList.add(boardMap);
 
         System.out.println("boardList = " + boardList);
-
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("resultCode", 200);
-
-        return resultMap;
+        */
     }
 
     @GetMapping("/list")
-    public List<Map<String, Object>> list() {
-        return boardList;
+    public List<Board> list() {
+//        return boardList;
+        return boardService.listBoard();
     }
 
     @GetMapping("/detail")
@@ -53,6 +67,23 @@ public class BoardRestController {
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("resultCode", 200);
         resultMap.put("detail_board", boardMap);
+
+        return resultMap;
+    }
+
+    @GetMapping("/update")
+    public Map<String, Object> update(@RequestParam Map<String, Object> params) {
+
+        int index = Integer.parseInt(params.get("order") + "") - 1;
+        Map<String, Object> boardMap = boardList.get(index);
+        boardMap.put("title", params.get("title"));
+        boardMap.put("content", params.get("content"));
+        boardMap.put("author", params.get("author"));
+        //boardList.add
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("resultCode", 200);
+        //resultMap.put("detail_board", boardMap);
 
         return resultMap;
     }
