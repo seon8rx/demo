3ff86c31f670547e3959a49f74309009cdc4baef
SUNGKYU KIM <seooonggyu@gmail.com>
2024-10-01 19:13:29 +0900

    24-02 spring session (09/30)

--- src/main/java/com/example/demo/controller/page/NoticeController.java
+++ src/main/java/com/example/demo/controller/page/NoticeController.java
@@ -1,4 +1,22 @@
 package com.example.demo.controller.page;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice")
 public class NoticeController {
+
+    @GetMapping("/{page}")
+    public String page(@PathVariable String page){
+        return "notice/" + page;
+    }
+
+    @GetMapping("/{page}/{id}")
+    public String page2(@PathVariable String page, @PathVariable String id){
+        return "notice/" + page;
+    }
+
 }
