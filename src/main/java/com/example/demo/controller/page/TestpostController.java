7f9486de65fd0e8329819a9eb61f84f768f6c8af
SUNGKYU KIM <seooonggyu@gmail.com>
2025-02-04 19:15:32 +0900

    load img

--- src/main/java/com/example/demo/controller/page/TestpostController.java
+++ src/main/java/com/example/demo/controller/page/TestpostController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller.page;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
@@ -41,4 +42,27 @@ public class TestpostController {
         FileCopyUtils.copy(inputStream, response.getOutputStream());
     }
 
+    @ResponseBody
+    @GetMapping(value = "/image/{file_name:.+}")
+    public byte[] getImage(@PathVariable("file_name") String file_name) throws Exception {
+        String root_path = "/Users/seooonggyu/Documents/testpostFileUpload/";
+        byte[] return_byte = null;
+
+        File file = new File(root_path + file_name);
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            return_byte = IOUtils.toByteArray(inputStream);
+        } catch (FileNotFoundException e) {
+        } catch (IOException e){
+        } finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+        return return_byte;
+    }
 }
